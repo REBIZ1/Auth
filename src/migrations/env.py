37ab98cde2767c16c4f73fe5e73d0c619a3aec78
Arthur <arthur155188@gmail.com>
2026-06-15 19:16:47 +0500

    Alembic Init

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 
 from src.core.config import settings
 from src.core.database import Base
-from src.models import * # noqa
+from src.models import *  # noqa
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
