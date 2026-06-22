3a0b5a41ec1718fd1fb6a59414c83e43665b084d
Arthur <arthur155188@gmail.com>
2026-06-22 16:31:47 +0500

    Добавлен метод get_rule_by_id: Получает правило доступа по его идентификатору

--- src/repositories/access_rules.py
+++ src/repositories/access_rules.py
@@ -1,23 +1,34 @@
 from sqlalchemy import select
 
+from src.repositories.mappers.access_rules import AccessRuleDataMapper
 from src.models import AccessRuleOrm, BusinessElementOrm
 from src.repositories.base import BaseRepository
 
 
 class AccessRuleRepository(BaseRepository):
     model = AccessRuleOrm
+    mapper = AccessRuleDataMapper
 
     async def get_rule(self, role_id: int, element_name: str):
         """
-        Метод получения правила
+        Получает правило доступа для указанной роли и бизнес элемента
         """
         query = (
-            select(AccessRuleOrm)
-            .join(BusinessElementOrm, AccessRuleOrm.element_id == BusinessElementOrm.id)
+            select(self.model)
+            .join(BusinessElementOrm, self.model.element_id == BusinessElementOrm.id)
             .where(
-                AccessRuleOrm.role_id == role_id,
+                self.model.role_id == role_id,
                 BusinessElementOrm.name == element_name,
             )
         )
         result = await self.session.execute(query)
         return result.scalar_one()
+
+    async def get_rule_by_id(self, rule_id: int):
+        """
+        Получает правило доступа по его идентификатору
+        """
+        query = select(self.model).where(self.model.id == rule_id)
+        result = await self.session.execute(query)
+        obj = result.scalars().one()
+        return AccessRuleDataMapper.map_to_domain_entity(obj)
